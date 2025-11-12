d31fb57c69f4d18ce0762004c93742afd73ffc50
verhneudinec <webarts@yandex.ru>
2025-11-12 17:37:42 +0700

    * fix scroll on android

--- lib/drag_manager.dart
+++ lib/drag_manager.dart
@@ -160,7 +160,8 @@ class DragAndDropListsMananger {
     // Adjust bottom boundary to account for iOS safe area
     final bottomPercentage = isLandscape && !isTablet
       ? 1.60  // 160% for mobile in landscape
-      : Platform.isIOS ? 0.99 : 0.105; // 99% for portrait iOS, 105% for portrait Android
+      : (Platform.isIOS ? 0.99 : 1.03); // 99% for portrait iOS, 103% for portrait Android
+
     final top = viewportHeight * topPercentage;
     final bottom = (viewportHeight * bottomPercentage) - bottomSafeArea;
 
